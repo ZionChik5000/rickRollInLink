31cfc952b2c6cfc696230c9364c3f5c19faa4bf6
ZionChik5000 <supercreative5000@gmail.com>
2024-12-24 23:34:04 +0200

    fixed small button
    maybe. ill check

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const video = document.getElementById("fullscreenVideo");
 const button = document.getElementById("startButton");
+const overlay = document.getElementById("overlay");
 
-button.addEventListener("click", () => {
-  button.style.display = "none";
+document.body.addEventListener("click", () => {
+  overlay.style.display = "none";
   video.style.display = "block";
   video.play();
 });
